2940b803f83aaee1ffd5756caf33ccfab6b6634d
ermalwebdev <101928596+ermalwebdev@users.noreply.github.com>
2022-06-14 11:56:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ hamburger.addEventListener("click", function () {
     closeIcon.style.display = "none";
   } else {
     menuMobile.classList.add("showMenu");
-    closeIcon.style.display = "block!important";
+    closeIcon.style.display = "block";
     openIcon.style.display = "none";
   }
 });
